3798b139e7dfdf6739cb2b4068ccb84de8b8fdbe
Abraham Morillo <abrahammorillo777@gmail.com>
2020-02-24 19:13:13 -0400

    Styles in App xaml

--- ContactAppSample/ContactAppSample.iOS/AppDelegate.cs
+++ ContactAppSample/ContactAppSample.iOS/AppDelegate.cs
@@ -24,7 +24,7 @@ namespace ContactAppSample.iOS
         {
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
-
+            Rg.Plugins.Popup.Popup.Init();
             return base.FinishedLaunching(app, options);
         }
     }
--- ContactAppSample/ContactAppSample/ViewModels/ListContactViewModel.cs
+++ ContactAppSample/ContactAppSample/ViewModels/ListContactViewModel.cs
@@ -1,10 +1,26 @@
-﻿using System;
+﻿using ContactAppSample.Views;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace ContactAppSample.ViewModels
 {
-    class ListContactViewModel
+    public class ListContactViewModel: INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand Addbtn { get; set; }
+
+        public ListContactViewModel()
+        {
+            Addbtn = new Command(AddPeople);
+        }
+        private async void AddPeople()
+        {
+            await App.Current.MainPage.Navigation.PushAsync(new AddContactPage());
+        }
     }
 }
