5751ad78380524bfeef51c0942cf1c8c649dd700
Abraham Morillo <abrahammorillo777@gmail.com>
2020-02-24 22:42:24 -0400

    Binding commands

---
+++ ContactAppSample/ContactAppSample/Models/People.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactAppSample.Models
+{
+    public class People
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Telefono { get; set; }
+        public string Celular { get; set; }
+    }
+}
---
+++ ContactAppSample/ContactAppSample/ViewModels/AddContactViewModel.cs
@@ -0,0 +1,35 @@
+﻿using ContactAppSample.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ContactAppSample.ViewModels
+{
+    public class AddContactViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public People People { get; set; } = new People();
+        public ICommand AddPeoplebtn { get; set; }
+
+        public AddContactViewModel()
+        {
+            AddPeoplebtn = new Command(AddContact);
+        }
+
+        private async void AddContact()
+        {
+            if (string.IsNullOrEmpty(People.Nombre) || string.IsNullOrEmpty(People.Apellido))
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "Empty fields", "Ok");
+            }
+            else
+            {
+                
+            }
+        }
+
+    }
+}
