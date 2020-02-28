24a5bc2f9057a93199db011989381ef967dada44
Abraham Morillo <abrahammorillo777@gmail.com>
2020-02-27 20:58:48 -0400

    Done

--- ContactAppSample/ContactAppSample/ViewModels/ListContactViewModel.cs
+++ ContactAppSample/ContactAppSample/ViewModels/ListContactViewModel.cs
@@ -13,25 +13,27 @@ using Xamarin.Forms.OpenWhatsApp;
 using System.Windows.Input;
 using Xamarin.Forms;
 using Plugin.Messaging;
+using System.Collections.ObjectModel;
 
 namespace ContactAppSample.ViewModels
 {
     public class ListContactViewModel: INotifyPropertyChanged
     {
-        Frame frame = new Frame();
+      
         
         public event PropertyChangedEventHandler PropertyChanged;
         public List<People> Peoples { get; set; } = new List<People>();
         public ICommand TapFrame { get; set; }
-        public ICommand Search { get; set; }
         public ICommand Addbtn { get; set; }
         public ICommand EditContact { get; set; }
         public ICommand DeleteContact { get; set; }            
         public ICommand Refresh { get; set; }
-        public string StartLetter { get; set; }
+        
         public bool State { get; set; } 
-        public bool StateList { get; set; }
+        
         public People People { get; set; } = new People();
+
+       
 
         public SQLiteConnection conn;
 
@@ -49,10 +51,8 @@ namespace ContactAppSample.ViewModels
             Addbtn = new Command(AddPeople);
 
             //TapFrame = new Command(TapGesture);
-            TapFrame = new Command(Tap);
-
-            
-           
+            TapFrame = new Command(Tap);           
+                       
             //Delete contact//
             DeleteContact = new Command<People>((sender) =>
             {                
@@ -128,6 +128,5 @@ namespace ContactAppSample.ViewModels
             }
         }
 
-       
     }
 }
